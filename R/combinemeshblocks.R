486d40f6340b3af3d0e6a2960f82558b035ee027
Angus <angus.moore@internode.on.net>
2017-10-29 15:22:15 +1100

    Correct file paths for loading meshblocks

--- R/combinemeshblocks.R
+++ R/combinemeshblocks.R
@@ -1,14 +1,14 @@
 loadAUSmb <- function(MBcode = "MB_CODE11") {
   message("Loading mesh blocks...")
-  ACT <- sf::st_read("data-raw/meshblocks/MB_2011_ACT.shp", type = 6)
-  NSW <- sf::st_read("data-raw/meshblocks/MB_2011_NSW.shp", type = 6)
-  NT <- sf::st_read("data-raw/meshblocks/MB_2011_NT.shp", type = 6)
-  OT <- sf::st_read("data-raw/meshblocks/MB_2011_OT.shp", type = 6)
-  QLD <- sf::st_read("data-raw/meshblocks/MB_2011_QLD.shp", type = 6)
-  SA <- sf::st_read("data-raw/meshblocks/MB_2011_SA.shp", type = 6)
-  TAS <- sf::st_read("data-raw/meshblocks/MB_2011_TAS.shp", type = 6)
-  VIC <- sf::st_read("data-raw/meshblocks/MB_2011_VIC.shp", type = 6)
-  WA <- sf::st_read("data-raw/meshblocks/MB_2011_WA.shp", type = 6)
+  ACT <- sf::st_read(system.file("extdata", "meshblocks/MB_2011_ACT.shp", package = "absgeoutils"), type = 6)
+  NSW <- sf::st_read(system.file("extdata", "meshblocks/MB_2011_NSW.shp", package = "absgeoutils"), type = 6)
+  NT <- sf::st_read(system.file("extdata", "meshblocks/MB_2011_NT.shp", package = "absgeoutils"), type = 6)
+  OT <- sf::st_read(system.file("extdata", "meshblocks/MB_2011_OT.shp", package = "absgeoutils"), type = 6)
+  QLD <- sf::st_read(system.file("extdata", "meshblocks/MB_2011_QLD.shp", package = "absgeoutils"), type = 6)
+  SA <- sf::st_read(system.file("extdata", "meshblocks/MB_2011_SA.shp", package = "absgeoutils"), type = 6)
+  TAS <- sf::st_read(system.file("extdata", "meshblocks/MB_2011_TAS.shp", package = "absgeoutils"), type = 6)
+  VIC <- sf::st_read(system.file("extdata", "meshblocks/MB_2011_VIC.shp", package = "absgeoutils"), type = 6)
+  WA <- sf::st_read(system.file("extdata", "meshblocks/MB_2011_WA.shp", package = "absgeoutils"), type = 6)
   message("Loading mesh blocks...done.")
 
   message("Merging mesh blocks...", appendLF = FALSE)
@@ -17,7 +17,7 @@ loadAUSmb <- function(MBcode = "MB_CODE11") {
 
   # Read in population counts
   message("Adding population counts...", appendLF = FALSE)
-  pop <- read.csv("data-raw/meshblocks/censuscounts_mb_2011_aust.csv", header = TRUE)
+  pop <- read.csv(system.file("extdata", "meshblocks/censuscounts_mb_2011_aust.csv", package = "absgeoutils"), header = TRUE)
   AUS <- merge(AUS, pop, by.x = MBcode, by.y = "Mesh_Block_ID", all.x = TRUE)
   message("done.")
 
