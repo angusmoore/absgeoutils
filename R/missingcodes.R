a88628f3e868cc7ce94bee9c4ef08d0ba2a5ecaa
angusmoore <angus.moore@internode.on.net>
2018-02-23 10:24:05 +1100

    Not used

--- R/missingcodes.R
+++
@@ -1,9 +0,0 @@
-iscontained <- function(code, correspondence, id) {
-  return(code %in% correspondence[, id])
-}
-
-missingcodes <- function(original, new, id) {
-  codes <- unique(original[, id])
-  sf::st_geometry(codes) <- NULL
-  return(!sapply(codes, FUN = iscontained, correspondence = new, id = id))
-}
