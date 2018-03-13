e7267b73da37a82ca2a4fef3f9d6664a9c9c6b2f
angusmoore <angus.moore@internode.on.net>
2018-03-13 12:53:08 +1100

    Add bash script to automatically build and deploy docs to gh-pages (#5)

---
+++ .push_gh_pages.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+rm -rf out || exit 0;
+mkdir out;
+
+GH_REPO="@github.com/angusmoore/absgeoutils.git"
+
+FULL_REPO="https://$GH_TOKEN$GH_REPO"
+
+for files in '*.tar.gz'; do
+        tar xfz $files
+done
+
+R -e "path <- '../absgeoutils'; system(paste(shQuote(file.path(R.home('bin'), 'R')), 'CMD', 'Rd2pdf', shQuote(path)))"
+
+R CMD INSTALL "$(find . -type f -iname 'absgeoutils*.tar.gz' -print -quit)"
+
+cd out
+git init
+git config user.name "travis"
+git config user.email "travis"
+
+cp ../absgeoutils.pdf absgeoutils.pdf
+
+git add .
+git commit -m "Auto-deploy docs to github pages"
+git push --force --quiet $FULL_REPO master:gh-pages
